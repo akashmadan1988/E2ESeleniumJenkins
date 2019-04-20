4ac230786d3e546ca8a64abddc8cc31215b4149b
HP <HP@HP-PC>
2019-04-20 19:35:45 +0200

    Created automation project

---
+++ E2ESeleniumJenkins/src/TestCases/TC_002.java
@@ -0,0 +1,21 @@
+package TestCases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_002 {
+	@Test
+	public void tc_001()
+	{
+		
+		System.setProperty("webdriver.chrome.driver","./Driver/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.findElement(By.id("email")).sendKeys("hello");
+		driver.findElement(By.id("pass")).sendKeys("hello");
+		driver.quit();
+	}
+
+}
